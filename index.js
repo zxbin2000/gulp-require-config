dd42d474c6a47b325c5561f750a9a9a255d2956a
zxbin2000 <zxbin2000@gmail.com>
2015-08-13 22:28:35 +0800

    require config

--- index.js
+++ index.js
@@ -7,40 +7,39 @@ var path         = require('path');
 
 var PLUGIN_NAME  = 'gulp-require-config';
 
-function _getManifestData(file, opts) {
-    var data = null;
-    var ext = path.extname(file.path);
-    if (ext === '.json') {
-        var json = {};
-        try {
-            var content = file.contents.toString('utf8');
-            if (content) {
-                json = JSON.parse(content);
-            }
-        } catch (x) {
-            this.emit('error', new PluginError(PLUGIN_NAME,  x));
-            return;
+var defaults = {
+};
+
+function _getManifestData(file) {
+    var data = '';
+    var json = {};
+    try {
+        var content = file.contents.toString('utf8');
+        if (content) {
+            json = JSON.parse(content);
         }
-        if (_.isObject(json)) {
-            Object.keys(json).forEach(function (key) {
-                data += key + ':' + json[key] + ',\n'
-            });
-        }
-        
+    } catch (x) {
+        this.emit('error', new PluginError(PLUGIN_NAME,  x));
+        return;
     }
-    return data;
+    if (_.isObject(json)) {
+        Object.keys(json).forEach(function (key) {
+            data += "\t\t'" + key + "': '" + json[key] + "',\n";
+        });
+    }
+    return data.slice(0, -2);
 }
 
-function revCollector(opts) {
+function revConfig(opts) {
     opts = _.defaults((opts || {}), defaults);
-    
+
     var manifest = '';
     var mutables = [];
     return through.obj(function (file, enc, cb) {
         if (!file.isNull()) {
-            var mData = _getManifestData.call(this, file, opts);
-            if (mData) {
-                manifest = mData;
+            var ext = path.extname(file.path);
+            if(ext === '.json') {
+                manifest = _getManifestData.call(this, file, opts);
             } else {
                 mutables.push(file);
             }
@@ -50,14 +49,14 @@ function revCollector(opts) {
         mutables.forEach(function (file){
             if (!file.isNull()) {
                 var src = file.contents.toString('utf8');
-                src = src.replace("//@gulp-md5", manifest);
+                src = src.replace("'gulp-md5' : ''", manifest);
                 file.contents = new Buffer(src);
             }
             this.push(file);
         }, this);
-        
+
         cb();
     });
 }
 
-module.exports = revCollector;
+module.exports = revConfig;
